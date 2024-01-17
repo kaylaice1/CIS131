6d7e46762896820c7de3a83e310a3fd8fd0c740b
Kaylaice <johnson.kaylynn@outlook.com>
2024-01-17 11:03:55 -0600

    s

--- Portfolio/new.js
+++ Portfolio/new.js
@@ -4,6 +4,8 @@ let myNumber = 5;
 myNumber = myNumber * 5;
 
 let userValue = prompt("What is your favorite number?");
+console.log("myNumber:", myNumber);
+console.log("userValue:", userValue);
 
 function processNumber(value) {
     if (value % 2 !== 0) {
@@ -17,6 +19,3 @@ let startNumber = 7;
 for (let i = 0; i < 5; i++) {
     startNumber = processNumber(startNumber);
 }
-
-console.log("myNumber:", myNumber);
-console.log("userValue:", userValue);
