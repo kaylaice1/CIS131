ea0383991bce8740a629cbed8facb6cbc9682560
Kaylaice <150838388+Kaylaice@users.noreply.github.com>
2024-01-17 10:27:52 -0600

    asf

--- Portfolio/new.js
+++ Portfolio/new.js
@@ -1,1 +1,6 @@
 //KayLynn Johnson 01/17/23
+let myNumber = 5;
+myNumber = myNumber * 5;
+let userValue = prompt("What is your favorite number?");
+console.log("myNumber:", myNumber);
+console.log("userValue:", userValue);
