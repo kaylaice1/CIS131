318f513cb69d38d4a7094e6ef1c0de26e97a5605
Kaylaice <johnson.kaylynn@outlook.com>
2024-03-12 08:43:22 -0500

    rain

--- Portfolio/Rainbowbirthday/rainbow.js
+++ Portfolio/Rainbowbirthday/rainbow.js
@@ -1,26 +1,27 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+let slideIndex = 1;
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+        slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+    slides[slideIndex - 1].style.display = "block";
 }
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Display the first slide when the page loads
+showSlides(slideIndex);
