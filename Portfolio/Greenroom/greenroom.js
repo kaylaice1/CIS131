d11179e78164911b86a9d62f447568f4b6121bde
Kaylaice <johnson.kaylynn@outlook.com>
2024-03-12 13:22:13 -0500

    ds

--- Portfolio/Greenroom/greenroom.js
+++ Portfolio/Greenroom/greenroom.js
@@ -1,11 +1,11 @@
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
-const output = document.getElementById('output');
+const output1 = document.getElementById('output1');
+const output2 = document.getElementById('output2');
 
-input1.addEventListener('input', updateOutput);
-input2.addEventListener('input', updateOutput);
+input1.addEventListener('input', updateOutput.bind(null, input1, output1));
+input2.addEventListener('input', updateOutput.bind(null, input2, output2));
 
-function updateOutput() {
-    output.innerHTML = "Text from Input 1: " + input1.value + "<br>" +
-                       "Text from Input 2: " + input2.value;
+function updateOutput(input, output) {
+    output.textContent = input.value;
 }
